32622521b6954af1bd784ebeef66ead8fe6fd43a
Deepak Malkan <Deepak.Malkan@bentley.com>
2021-07-02 14:20:30 -0400

    Develop the maskUSPhone feature. Change .eslintrc.json file to fix lint import/export error message. Not sure why package-lock.json is updated.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
-function sayHiTo (name) {
-    return `Hi, ${name}`;
+const digitsOnly = /^\d+$/;
+
+//function to mask digits into US phone format
+function maskUSPhone (phone) {
+    //return null when getting null, undefined, or an object that is not a string
+    if (!phone || typeof phone !== 'string') {
+        return null;
+    }
+
+    // return the untouched value when it contins non-digit chars or when it has a length != 10
+    if (digitsOnly.test (phone) === false || phone.length !== 10) {
+        return phone;
+    }
+
+    // return the masked value
+    const areaCode = phone.substring (0, 3);
+    const prefix = phone.substring (3, 6);
+    const suffix = phone.substring (6, 10);
+
+    return `(${areaCode}) ${prefix}-${suffix}`;
 }
 
-const message = sayHiTo ('Bruno');
-
-console.log (message);
+export default maskUSPhone;
